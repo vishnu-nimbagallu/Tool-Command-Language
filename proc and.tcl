3230085f2ac78fbff57efa11f386ab4950d7403e
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-09-24 21:25:15 +0530

    Delete proc and.tcl

--- proc and.tcl
+++
@@ -1,8 +0,0 @@
-proc and {a b} {
-return [expr $a & $b]
-}
-puts "input1 \t input2 \t output"
-for {set i 0} {$i < 2} {incr i} {
-for {set j 0} {$j < 2} {incr j} {
-puts "$i\t $j\t [and $i $j]"
-} }
