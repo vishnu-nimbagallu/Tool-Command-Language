0253b02b6aa0499e226d9856972438df8b6513d4
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-10-22 22:12:44 +0530

    Add files via upload

---
+++ Scale-Range/scale range.tcl
@@ -0,0 +1,9 @@
+scale .s1 -orien h -from 0 -to 100 -length 100 -variable var1
+pack .s1
+label .l1 -text "value of scrollbar ="
+label .l2 -textvariable var1
+pack .l1 .l2
+scale .s2
+pack .s2
+ 
+ 
