9d9df4472e8e96925ca6d4ba523d4b6321dec5fe
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-10-29 23:10:58 +0530

    Add files via upload

---
+++ Place Manager/Placemanager ex2.tcl
@@ -0,0 +1,15 @@
+button .b1 -text A 
+
+button .b2 -text B
+
+button .b3 -text C
+
+button .b4 -text D
+
+place .b1 -relx 0.0 -rely 0.0
+
+place .b2 -relx 0.25 -rely 0.25
+
+place .b3 -relx 0.5 -rely 0.4
+
+place .b4 -relx 0.8 -rely 0.9
