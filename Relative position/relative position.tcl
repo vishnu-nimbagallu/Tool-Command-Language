3f2c64c978917bfcc5fdb37b69597fc9bc43d37a
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-11-29 22:45:05 +0530

    Add files via upload

---
+++ Relative position/relative position.tcl
@@ -0,0 +1,9 @@
+button .b1 -text M
+button .b2 -text E
+button .b3 -text E
+button .b4 -text T
+
+place .b1 -relx 0.0 -rely 0.0
+place .b2 -relx 0.25 -rely 0.25
+place .b3 -relx 0.5 -rely 0.4
+place .b4 -relx 0.8 -rely 0.9
