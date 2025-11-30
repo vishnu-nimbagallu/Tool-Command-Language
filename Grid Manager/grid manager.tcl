c8f0c5df4cc0b164afbf1ff223f304563bf349e1
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-11-30 23:39:51 +0530

    Add files via upload

---
+++ Grid Manager/grid manager.tcl
@@ -0,0 +1,8 @@
+frame .f1 -width 100 -height 100 -bg green
+frame .f2 -width 100 -height 100 -bg blue
+frame .f3 -width 100 -height 100 -bg red
+frame .f4 -width 100 -height 100 -bg yellow
+grid .f1 -row 0 -column 0
+grid .f2 -row 0 -column 1
+grid .f3 -row 1 -column 0
+grid .f4 -row 1 -column 1
