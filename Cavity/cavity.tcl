0355171ef22444c7ddbd143de6caffaf9a3d3004
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-12-07 22:49:19 +0530

    Add files via upload

---
+++ Cavity/cavity.tcl
@@ -0,0 +1,7 @@
+ frame .f1 -width 50 -height 100 -bg blue
+ frame .f2 -width 100 -height 150 -bg green
+ frame .f3 -width 150 -height 200 -bg yellow
+ pack .f1 -side right
+ pack .f2 -side left
+ pack .f3 -side bottom
+
